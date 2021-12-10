01b1b41f6c7f15fa99feb66ab2bb429a6f7c6640
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-12-10 21:50:27 +0100

    Add files via upload

---
+++ app2/app2/KlasaB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2
+{
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string stringUnos) 
+        { 
+            return stringUnos.Substring(1, stringUnos.Length- 2); 
+        
+        }
+    }
+}
---
+++ app2/app2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace app2
+{
+    class Program
+    {
+        /*
+         Definirajte klasu KlasaB sa sljedećim članicama:
+• metoda
+string BezPrvogIZadnjeg(string) koja će vratiti proslijeñeni string bez prvog i
+zadnjeg znaka.
+Definirajte objekt tipa KlasaB, zatražite od korisnika da unese niz znakova, te taj niz znakova
+proslijedite metodi BezPrvogIZadnjeg, a povratnu vrijednost ispišite. Osigurajte pritom da se
+metoda pozove samo ako je korisnik unio više od 2 znaka.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:"); 
+            
+            string s = Console.ReadLine(); 
+            
+            KlasaB x = new KlasaB();
+            if (s.Length > 2)
+            {
+                s = x.BezPrvogIZadnjeg(s); 
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
